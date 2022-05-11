0c9db7e6a162173fde92c1165c63e5f6b2a2f170
Bodin <bodintiago@gmail.com>
2022-05-11 20:17:16 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,37 @@
+from typing import Optional, List
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+items_db = [
+    {
+    "id": 1,
+    "name": "orange",
+    "price": 2
+    }
+]
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Optional[bool] = None
+
+
+@app.get("/items", response_model=List[Item])
+async def read_items():
+    return items_db
+
+
+@app.post("/items", status_code=201)
+async def create_item(payload: Item):
+    item = payload.dict()
+    items_db.append(item)
+    return items_db[-1]
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Optional[str] = None):
+    return [item for item in items_db if item_id == item["id"]]
